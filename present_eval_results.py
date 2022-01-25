5c73df678ee4fdd60675c18c8e715baa07a582c6
Eduard Belov <edbons@gmail.com>
2022-01-25 18:44:46 +0300

    eval results

--- present_eval_results.py
+++ present_eval_results.py
@@ -9,22 +9,25 @@ METRICS = ['ms_jaccard', 'tfidf_distance', 'frechet_bert_distance', 'forward_bac
 def main(args):
     experiments = os.listdir(args.output_dir)
 
+    results = []
     for experiment in experiments:
         log_dir = os.path.join(args.output_dir, experiment, 'eval_logs')     
         
-        results = []
+        exp_results=[]
         for metric in METRICS:
             metric_results = {}
             filenames = glob(f'{log_dir}/{metric}*.pickle')
             for name in filenames:
-                label = name.strip(f'{metric}_').strip('.pickle').strip()
+                label = f"{experiment}_{os.path.basename(name).replace(f'{metric}_','').strip('.pickle').strip()}"
                 with open(name, 'rb') as f:
                     metric_results[label]=pickle.load(f)
-            results.append(pd.DataFrame.from_dict(metric_results, orient='index')) 
+            exp_results.append(pd.DataFrame.from_dict(metric_results, orient='index')) 
         
-        df = pd.concat(results, axis=1)
-        print(df)
-
+        results.append(pd.concat(exp_results, axis=1))
+    
+    df = pd.concat(results, axis=0)
+    df.drop(['n_ref', 'n_hypo'], axis=1, inplace=True)
+    df.to_csv('experiments_results.csv', encoding='utf-8', sep='|')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
